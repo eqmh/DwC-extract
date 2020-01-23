3c1811ab1b3c490d5981c506f8aaa430ddba7727
ENRIQUE MONTES <emontesh@mail.usf.edu>
2020-01-23 12:01:17 -0500

    created spp lists

--- unique_spp.R
+++ unique_spp.R
@@ -3,16 +3,15 @@
 #### Enrique Montes
 #### January 21, 2020
 
-library(tidyverse)
-
 rm(list=ls()) ## clear variables
 
 setwd("~/DwC-extract/data")
 
-Occurrence <- read.csv("COLOMBIA-ISLA GORGONA_ipt_occurrence.csv", header = TRUE)
+Occurrence <- read.csv("ECUADOR-GALAPAGOS-ALL_ipt_occurrence.csv", header = TRUE)
 
 sppList <- unique(select(Occurrence, scientificName))
 
+write.csv(sppList, file = "~/DwC-extract/spp_lists/ECUADOR-GALAPAGOS-ALL_spp_list.csv", row.names = FALSE)
 
 ## Write the names to a text file
 # writeLines(con="COLOMBIA-ISLA GORGONA_sppList.txt", sppList)
